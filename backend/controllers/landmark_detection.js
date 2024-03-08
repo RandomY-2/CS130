6665b2af238264a6c40b07a99588ff7401bc7c16
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2024-03-08 15:00:41 -0800

    Revert "fix lint with standard --fix"
    This reverts commit 4660acb19b9314f75d1df794bbb51efadeb92107.

--- backend/controllers/landmark_detection.js
+++ backend/controllers/landmark_detection.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const GCPLandmarkSingleton = require('../third_party/google_lens')
-const FirebaseSingleton = require('../third_party/db')
-const firebaseInstance = FirebaseSingleton.getInstance()
-const db = firebaseInstance.getDatabase()
+const fs = require('fs');
+const GCPLandmarkSingleton = require('../third_party/google_lens');
+const FirebaseSingleton = require('../third_party/db');
+const firebaseInstance = FirebaseSingleton.getInstance();
+const db = firebaseInstance.getDatabase();
 
 /**
  * Converts degrees to radians
@@ -10,7 +10,7 @@ const db = firebaseInstance.getDatabase()
  * @returns {number} Radians
  */
 const degreesToRadians = (degrees) => {
-  return degrees * (Math.PI / 180)
+    return degrees * (Math.PI / 180);
 }
 
 /**
@@ -22,97 +22,97 @@ const degreesToRadians = (degrees) => {
  * @returns {number} The distance in kilometers
  */
 const haversineDistance = (lat1, lon1, lat2, lon2) => {
-  const R = 6371 // Radius of the Earth in kilometers
-  const dLat = degreesToRadians(lat2 - lat1)
-  const dLon = degreesToRadians(lon2 - lon1)
-  const a =
+    const R = 6371; // Radius of the Earth in kilometers
+    const dLat = degreesToRadians(lat2 - lat1);
+    const dLon = degreesToRadians(lon2 - lon1);
+    const a =
         Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos(degreesToRadians(lat1)) * Math.cos(degreesToRadians(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2)
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
-  return R * c
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
 }
 
 const isLandmarkCloseToUser = (userLatitude, userLongitude, landmarkCoords, threshold) => {
-  const distance = haversineDistance(userLatitude, userLongitude, landmarkCoords.latitude, landmarkCoords.longitude)
-  console.log(distance)
-  return distance <= threshold
+    const distance = haversineDistance(userLatitude, userLongitude, landmarkCoords.latitude, landmarkCoords.longitude);
+    console.log(distance);
+    return distance <= threshold;
 }
 
 module.exports.detectLandmark = async (req, res) => {
-  try {
-    console.log('recevied a landmark detection req', req)
+    try {
+        console.log('recevied a landmark detection req', req);
 
-    const landmarkDetector = GCPLandmarkSingleton.getInstance()
-    const filePath = req.file.path
-    const { latitude, longitude } = req.body
+        const landmarkDetector = GCPLandmarkSingleton.getInstance();
+        const filePath = req.file.path;
+        const {latitude, longitude} = req.body;
 
-    console.log('user coords', { latitude, longitude })
+        console.log('user coords', {latitude, longitude});
 
-    const result = await landmarkDetector.detectLandmark(filePath)
+        const result = await landmarkDetector.detectLandmark(filePath);
 
-    // coords = {'latitude': xx, 'longitude': xx}
-    const coords = result.landmarkAnnotations.map(annotation => annotation.locations[0].latLng)
-    const landmarks = result.landmarkAnnotations.map(annotation => annotation.description)
+        // coords = {'latitude': xx, 'longitude': xx}
+        const coords = result.landmarkAnnotations.map(annotation => annotation.locations[0].latLng);
+        const landmarks = result.landmarkAnnotations.map(annotation => annotation.description);
 
-    console.log('Landmark Detection Result:', result)
-    console.log('Landmark Detection Landmarks:', landmarks)
-    console.log('Landmark coords:', coords)
+        console.log("Landmark Detection Result:", result);
+        console.log("Landmark Detection Landmarks:", landmarks);
+        console.log("Landmark coords:", coords);
 
-    const closeLandmarks = landmarks.filter((_, index) => {
-      const landmarkCoords = coords[index]
-      console.log(landmarkCoords)
+        const closeLandmarks = landmarks.filter((_, index) => {
+            const landmarkCoords = coords[index];
+            console.log(landmarkCoords);
 
-      const threshold = 15 // 15km
-      return isLandmarkCloseToUser(latitude, longitude, landmarkCoords, threshold)
-    })
+            const threshold = 15; // 15km
+            return isLandmarkCloseToUser(latitude, longitude, landmarkCoords, threshold);
+        });
 
-    console.log('Close landmarks:', closeLandmarks)
+        console.log('Close landmarks:', closeLandmarks);
 
-    // Create new forum if not exist
-    if (closeLandmarks.length > 0) {
-      const forumLandmark = closeLandmarks[0]
+        // Create new forum if not exist
+        if (closeLandmarks.length > 0) {
+            const forumLandmark = closeLandmarks[0];
 
-      const forumsRef = db.collection('forums')
-      const Snapshot = await forumsRef.where('name', '==', forumLandmark).limit(1).get()
+            const forumsRef = db.collection('forums');
+            const Snapshot = await forumsRef.where('name', '==', forumLandmark).limit(1).get();
+            
+            let forumDocId;
 
-      let forumDocId
+            // Check if the forum (landmark) exists, if not create one
+            if (Snapshot.empty) {
+                // No existing forum found, create a new one
+                const newForumRef = forumsRef.doc(); // Automatically generate a new document ID
+                await newForumRef.set({
+                    name: forumLandmark,
+                    totalScore: 0,
+                    totalUsers: 0,
+                    // Don't set an optional rating initially
+                });
+                forumDocId = newForumRef.id;
+            } else {
+                forumDocId = Snapshot.docs[0].id;
+            }
 
-      // Check if the forum (landmark) exists, if not create one
-      if (Snapshot.empty) {
-        // No existing forum found, create a new one
-        const newForumRef = forumsRef.doc() // Automatically generate a new document ID
-        await newForumRef.set({
-          name: forumLandmark,
-          totalScore: 0,
-          totalUsers: 0
-          // Don't set an optional rating initially
-        })
-        forumDocId = newForumRef.id
-      } else {
-        forumDocId = Snapshot.docs[0].id
-      }
+            console.log('Current forum: ', forumDocId);
 
-      console.log('Current forum: ', forumDocId)
+            // Now check if this user has access to this forum in their AccessibleForums subcollection
+            const accessibleForumsRef = db.collection('users').doc(req.user.id).collection('AccessibleForums').doc(forumDocId);
+            const accessibleForumDoc = await accessibleForumsRef.get();
 
-      // Now check if this user has access to this forum in their AccessibleForums subcollection
-      const accessibleForumsRef = db.collection('users').doc(req.user.id).collection('AccessibleForums').doc(forumDocId)
-      const accessibleForumDoc = await accessibleForumsRef.get()
+            if (!accessibleForumDoc.exists) {
+                await accessibleForumsRef.set({ hasAccess: true }); 
+            }
+        }
 
-      if (!accessibleForumDoc.exists) {
-        await accessibleForumsRef.set({ hasAccess: true })
-      }
+        res.status(200).json({landmarks: closeLandmarks});
+    } catch (error) {
+        console.error("Error detecting landmarks:", error);
+        res.status(500).json({error});
+    } finally {
+        if (req.file && req.file.path) {
+            fs.unlinkSync(req.file.path);
+        }
+
+        console.log('removed uploaded file');
     }
-
-    res.status(200).json({ landmarks: closeLandmarks })
-  } catch (error) {
-    console.error('Error detecting landmarks:', error)
-    res.status(500).json({ error })
-  } finally {
-    if (req.file && req.file.path) {
-      fs.unlinkSync(req.file.path)
-    }
-
-    console.log('removed uploaded file')
-  }
 }
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,134 +1,134 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const FirebaseSingleton = require('../third_party/db')
-const firebaseInstance = FirebaseSingleton.getInstance()
-const db = firebaseInstance.getDatabase()
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const FirebaseSingleton = require('../third_party/db');
+const firebaseInstance = FirebaseSingleton.getInstance();
+const db = firebaseInstance.getDatabase();
 
 // Helper function to generate a JWT token
 const generateToken = (user) => {
-  return jwt.sign({ email: user.email, id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' })
-}
+  return jwt.sign({ email: user.email, id: user.id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
 
 module.exports.register = async (req, res) => {
-  const { email, password, name } = req.body
-  console.log('received register request', { email, password, name })
+    const { email, password, name } = req.body;
+    console.log('received register request', {email, password, name});
 
-  try {
-    const usersRef = db.collection('users')
-    const snapshot = await usersRef.where('email', '==', email).get()
+    try {
+      const usersRef = db.collection('users');
+      const snapshot = await usersRef.where('email', '==', email).get();
 
-    if (!snapshot.empty) {
-      console.log('found existing user with same email')
+      if (!snapshot.empty) {
+        console.log("found existing user with same email");
 
-      res.status(400).json({ message: 'Email already in use' })
-      return
+        res.status(400).json({ message: 'Email already in use' });
+        return;
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const newUserRef = db.collection('users').doc();
+      await newUserRef.set({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      console.log("created db entry for", {email, password, name});
+
+      const token = generateToken({ email, id: newUserRef.id });
+
+      console.log("created token for", {email, password, name});
+
+      res.status(200).json({ token, email, name });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 12)
-    const newUserRef = db.collection('users').doc()
-    await newUserRef.set({
-      name,
-      email,
-      password: hashedPassword
-    })
-
-    console.log('created db entry for', { email, password, name })
-
-    const token = generateToken({ email, id: newUserRef.id })
-
-    console.log('created token for', { email, password, name })
-
-    res.status(200).json({ token, email, name })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: error.message })
-  }
-}
+};
 
 module.exports.signin = async (req, res) => {
-  const { email, password } = req.body
-  console.log('received login request', { email, password })
+  const { email, password } = req.body;
+  console.log('received login request', {email, password});
 
   try {
-    const usersRef = db.collection('users')
-    const snapshot = await usersRef.where('email', '==', email).get()
+    const usersRef = db.collection('users');
+    const snapshot = await usersRef.where('email', '==', email).get();
 
     if (snapshot.empty) {
-      console.log('found existing user with same email')
+        console.log("found existing user with same email");
 
-      res.status(404).json({ message: 'User not found' })
-      return
+        res.status(404).json({ message: 'User not found' });
+        return;
     }
 
-    let user
+    let user;
     snapshot.forEach(doc => {
-      user = { id: doc.id, ...doc.data() }
-    })
+      user = { id: doc.id, ...doc.data() };
+    });
 
-    console.log('fetched user info for', { email, password })
+    console.log('fetched user info for', {email, password});
 
-    const isPasswordSame = await bcrypt.compare(password, user.password)
+    const isPasswordSame = await bcrypt.compare(password, user.password);
     if (!isPasswordSame) {
-      console.log('user password unmatch for', { email, password })
+        console.log("user password unmatch for", {email, password});
 
-      res.status(400).json({ message: 'Password Not Correct ' })
-      return
+        res.status(400).json({ message: 'Password Not Correct ' });
+        return;
     }
 
-    const token = generateToken(user)
+    const token = generateToken(user);
 
-    console.log('created token for', { email, password })
+    console.log("created token for", {email, password});
 
-    res.status(200).json({ token, email, name: user.name })
+    res.status(200).json({ token, email, name: user.name });
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: error.message })
+    console.log(error);
+    res.status(500).json({ message: error.message });
   }
-}
+};
 
 module.exports.getUsrDetailByEmail = async (req, res) => {
-  const { email } = req.params
+    const { email } = req.params;
 
-  try {
-    const usersRef = db.collection('users')
-    const snapshot = await usersRef.where('email', '==', email).get()
+    try {
+      const usersRef = db.collection('users');
+      const snapshot = await usersRef.where('email', '==', email).get();
 
-    if (snapshot.empty) {
-      res.status(404).json({ message: 'User Not Found' })
-      return
+      if (snapshot.empty) {
+        res.status(404).json({ message: 'User Not Found' });
+        return;
+      }
+
+      snapshot.forEach(doc => {
+        res.status(200).json(doc.data());
+      });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
     }
-
-    snapshot.forEach(doc => {
-      res.status(200).json(doc.data())
-    })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: error.message })
-  }
-}
+};
 
 module.exports.getUsrByToken = async (req, res) => {
   try {
-    console.log('get user by token for', req.user)
+    console.log("get user by token for", req.user);
 
-    const user = req.user
-    const usersRef = db.collection('users')
-    const snapshot = await usersRef.where('email', '==', user.email).get()
+    const user = req.user;
+    const usersRef = db.collection('users');
+      const snapshot = await usersRef.where('email', '==', user.email).get();
 
-    if (snapshot.empty) {
-      console.log('cannot find in db user: ', req.user)
+      if (snapshot.empty) {
+        console.log('cannot find in db user: ', req.user);
 
-      res.status(404).json({ message: 'User Not Found' })
-      return
-    }
+        res.status(404).json({ message: 'User Not Found' });
+        return;
+      }
 
-    snapshot.forEach(doc => {
-      const { email, name } = doc.data()
-      res.status(200).json({ email, name })
-    })
+      snapshot.forEach(doc => {
+        const {email, name} = doc.data();
+        res.status(200).json({email, name});
+      });
   } catch (error) {
-    console.log(error)
+    console.log(error);
 
-    res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message });
   }
-}
+};
